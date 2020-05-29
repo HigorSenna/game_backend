4fb1cd97ad3abeed861c40101ca7b460a9c9c3df
Higor Senna <higorrebjfmg@gmail.com>
2020-05-28 23:36:16 -0300

    Adding response_dto

---
+++ src/dto/response/user_response_dto.ts
@@ -0,0 +1,5 @@
+export class UserResponseDTO {
+    public id: number;
+    public name: string;
+    public email: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import {ServerStart} from './class/server_start';
 import { ServerSocketConstants } from './constants/server_sockets_constants';
 import { ClientSocketConstants } from './constants/client_sockets_constants';
+import { UserResponseDTO } from './dto/response/user_response_dto';
 
 const startServer: ServerStart = new ServerStart();
 startServer.startServer(3000);
@@ -8,9 +9,9 @@ startServer.startServer(3000);
 startServer.socketIo.on('connection', function(socket: SocketIO.Socket) {
     console.log('#### Connection Open ####');
 
-    socket.on('ping', function(data) {
+    socket.on('ping', function(data: UserResponseDTO) {
         console.log('### Processing Data ###');
-        console.log(data['message']);
+        console.log(data.name);
 
         const messageToSendServer = {
             message: 'pong message!!'
